8fcfb2b9f571affbc2203a4d4372dec3754ef31f
Jianglexing <1721900707@qq.com>
2022-01-19 11:34:25 +0800

    feature: 更新服务端逻辑用于使其返回当前时间

--- hellospring/src/main/java/com/sqlpy/hellospring/hellospring/HellospringApplication.java
+++ hellospring/src/main/java/com/sqlpy/hellospring/hellospring/HellospringApplication.java
@@ -5,13 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @SpringBootApplication
 public class HellospringApplication {
 
     @RequestMapping("/")
-    private String hello() {
-        return "hello-spring.";
+    private Map currentTime() {
+        Map<String,String> map = new HashMap<>();
+        Date currentTime = new Date();
+        SimpleDateFormat formater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        map.put("now",formater.format(currentTime));
+        return map;
     }
 
     public static void main(String[] args) {
